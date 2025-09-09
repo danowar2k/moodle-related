7fa54812b596a06b579e666af13fd83c2863fc50
Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de>
2025-09-09 16:40:16 +0200

    Split questions and subquestions

--- qtype_multianswer/database_consistency_check.php
+++ qtype_multianswer/database_consistency_check.php
@@ -6,6 +6,10 @@ global $DB;
 
 require_admin();
 
+$questionsWithProblems = [];
+$subquestionsWithProblems = [];
+$missingQuestions = [];
+
 echo "Checking qtype_multianswer database consistency...<br/>";
 echo "<br/>";
 echo "1) Does every multianswer record have a matching question record?<br/>";
@@ -22,6 +26,7 @@ $multianswersMissingQuestionsSql =
         WHERE q.id IS NULL";
 $qmaMissingQuestions = $DB->get_records_sql($multianswersMissingQuestionsSql);
 foreach ($qmaMissingQuestions as $qmaMissingQuestion) {
+    $missingQuestions[] = $qmaMissingQuestion->question;
     echo "1) Multianswer record with missing question: ".$qmaMissingQuestion->id."<br/>";
 }
 
@@ -34,6 +39,7 @@ if (!$maQuestionsWithParent) {
     echo "2) OK. No MA questions that have a parent but should not<br/>";
 } else {
     foreach ($maQuestionsWithParent as $maQuestionWithParent) {
+        $questionsWithProblems[] = $maQuestionWithParent->id;
         echo "Multianswer question with a parent: " . $maQuestionWithParent->id . "<br/>";
     }
 }
@@ -51,20 +57,23 @@ foreach ($multianswers as $multianswer) {
     $maId = $multianswer->id;
     if (!isset($multianswer->sequence) || is_null($multianswer->sequence) || !$multianswer->sequence) {
         echo "3a) MA record without a sequence found: ".$maId."<br/>";
+        $questionsWithProblems[] = $multianswer->question;
         continue;
     }
     $maSequence = $multianswer->sequence;
     $maSequenceQuestionIds = explode(',', $maSequence);
     if (!$maSequenceQuestionIds || preg_match("/^,+$/", $maSequence)) {
-        echo "3b) Bad sequence for MA (id: ".$maId.") found ("."$multianswer->sequence".")<br/>";
+        $questionsWithProblems[] = $multianswer->question;
+        echo "3b) Bad sequence for MA (id: $maId) found ($maSequence)<br/>";
         continue;
     }
     $sequenceCount = count($maSequenceQuestionIds);
 
-    $subQuestionSql = "SELECT * FROM {question} q WHERE q.id IN($multianswer->sequence)";
+    $subQuestionSql = "SELECT * FROM {question} q WHERE q.id IN($maSequence)";
     $subquestions = $DB->get_records_sql($subQuestionSql);
     $subQuestionCount = count($subquestions);
     if ($sequenceCount !== $subQuestionCount) {
+        $questionsWithProblems[] = $multianswer->question;
         echo "3c) MA with id: ".$maId."<br/>";
         echo "3c) Has sequence of ".$sequenceCount." subquestions (".$maSequence.")<br/>";
         echo "3c) But found only ".$subQuestionCount." of them in database<br/>";
@@ -73,7 +82,42 @@ foreach ($multianswers as $multianswer) {
         $subquestionId = $subquestion->id;
         $subquestionParent = $subquestion->parent;
         if ($subquestionParent !== $maId) {
-            "3d) Subquestion ".$subquestionId." is in sequence of MA with ID ".$maId." but has the wrong parent ".$subquestionParent."<br/>";
+            $questionsWithProblems[] = $multianswer->question;
+            $subquestionsWithProblems[] = $subquestionId;
+            echo "3d) Subquestion ".$subquestionId." is in sequence of MA with ID ".$maId." but has the wrong parent ".$subquestionParent."<br/>";
         }
+    }
+}
+$questionsWithProblems = array_unique($questionsWithProblems);
+$subquestionsWithProblems = array_unique($subquestionsWithProblems);
+
+$nrQuestionsWithProblems = count($questionsWithProblems);
+$nrSubquestionsWithProblems = count($subquestionsWithProblems);
+
+echo "Multianswer questions with problems: $nrQuestionsWithProblems<br/>";
+echo "Multianswer subquestion questions with problems: $nrSubquestionsWithProblems<br/>";
+
+if ($questionsWithProblems) {
+    echo "<br/>";
+    $sql = "SELECT q.id as questionid, c.id as courseid FROM {question} q
+    JOIN {question_versions} qv ON q.id = qv.questionid
+    JOIN {question_bank_entries} qbe ON qbe.id = qv.questionbankentryid
+    JOIN {question_categories} qc ON qc.id = qbe.questioncategoryid
+    JOIN {context} ctx ON ctx.id = qc.contextid
+    JOIN {course} c ON c.id = ctx.instanceid
+    WHERE ctx.contextlevel = 50
+    AND q.id IN(".implode(",", $questionsWithProblems).")";
+
+    $questionsAndCourses = $DB->get_records_sql($sql);
+    $nrResults = count($questionsAndCourses);
+    $foundQuestions = array_keys($questionsAndCourses);
+    $missingQuestions = array_diff($questionsWithProblems, $foundQuestions);
+
+    if ($nrQuestionsWithProblems > $nrResults) {
+        echo "Warning! There are problematic questions not directly in a course.<br/>";
+        echo "Questions not in a course: ".implode(",", $missingQuestions)."<br/>";
+    }
+    foreach ($questionsAndCourses as $questionAndCourse) {
+        echo "Problematic question ".$questionAndCourse->questionid." is in course ".$questionAndCourse->courseid."<br/>";
     }
 }
